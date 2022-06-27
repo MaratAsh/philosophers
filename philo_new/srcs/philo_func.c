d99f480569bb546972c4ad0166bb6e84ed92ff3f
Altagracia Cierra <alcierra@student.21-school.ru>
2022-06-27 21:32:46 +0300

    norminette

--- philo_new/srcs/philo_func.c
+++ philo_new/srcs/philo_func.c
@@ -12,9 +12,9 @@
 
 #include "../philo.h"
 
-int 	philo_need_stop(t_main *m, t_philo *p)
+int	philo_need_stop(t_main *m, t_philo *p)
 {
-	int force_stop;
+	int	force_stop;
 
 	pthread_mutex_lock(&(m->main_mutex));
 	force_stop = m->force_stop;
@@ -29,43 +29,41 @@ int 	philo_need_stop(t_main *m, t_philo *p)
 	return (0);
 }
 
+int	philo_func_loop(t_main *m, t_philo *p)
+{
+	int	status;
+
+	pthread_mutex_lock(&(p->mutex));
+	status = p->status;
+	if (status & PHILOSOPHER_STOP || status & PHILOSOPHER_DIE)
+	{
+		pthread_mutex_unlock(&(p->mutex));
+		return (1);
+	}
+	if (status == PHILOSOPHER_THINK)
+		philo_action_take_first_fork(m, p);
+	else if (status & PHILOSOPHER_EAT)
+		philo_action_sleep(m, p);
+	else if ((status & PHILOSOPHER_TAKE_FORK) && (status & FORK_2))
+		philo_action_eat(m, p);
+	else if ((status & PHILOSOPHER_TAKE_FORK) && (status & FORK_1))
+		philo_action_take_second_fork(m, p);
+	else if (status & PHILOSOPHER_SLEEP)
+		philo_action_think(m, p);
+	else
+		pthread_mutex_unlock(&(p->mutex));
+	return (0);
+}
+
 void	*philo_func(t_philo *p)
 {
-	t_main			*m;
-	int 			status;
+	t_main	*m;
 
 	m = p->parent;
 	while (!philo_need_stop(m, p))
 	{
-		pthread_mutex_lock(&(p->mutex));
-		status = p->status;
-		if (status & PHILOSOPHER_STOP || status & PHILOSOPHER_DIE)
-		{
-			pthread_mutex_unlock(&(p->mutex));
-			break;
-		}
-		if (status == PHILOSOPHER_THINK)
-		{
-			philo_action_take_first_fork(m, p);
-		}
-		else if (status & PHILOSOPHER_EAT)
-		{
-			philo_action_sleep(m, p);
-		}
-		else if ((status & PHILOSOPHER_TAKE_FORK) && (status & FORK_2))
-		{
-			philo_action_eat(m, p);
-		}
-		else if ((status & PHILOSOPHER_TAKE_FORK) && (status & FORK_1))
-		{
-			philo_action_take_second_fork(m, p);
-		}
-		else if (status & PHILOSOPHER_SLEEP)
-		{
-			philo_action_think(m, p);
-		}
-		else
-			pthread_mutex_unlock(&(p->mutex));
+		if (philo_func_loop(m, p))
+			break ;
 	}
 	return (0);
 }
